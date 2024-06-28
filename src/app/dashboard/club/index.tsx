c646399116116bffd990150b438b9876440a3584
squaremost <egghead.kenzo@gmail.com>
2024-06-29 08:58:09 +0900

    club page

--- src/app/dashboard/club/index.tsx
+++ src/app/dashboard/club/index.tsx
@@ -2,7 +2,7 @@ import { Pressable, View, Text } from "react-native";
 import QRCode from "react-qr-code";
 import { FontAwesome } from "@expo/vector-icons";
 import { Container } from "@/components";
-import { Ticket, UserRound, UsersRound } from "lucide-react-native";
+import { Group, Search, Settings, Ticket, UserRound, UsersRound } from "lucide-react-native";
 import { useAppSelector } from "@/store/hook";
 
 const Club: React.FC = () => {
@@ -13,30 +13,42 @@ const Club: React.FC = () => {
   return (
     <Container>
       <View className="px-5">
-        <View className="flex flex-col items-center bg-white rounded-3xl mb-4 py-8">
-          <QRCode
-            size={120}
-            style={{ height: "auto", maxWidth: 50, width: 50 }}
-            value={"sdfsdfsdf"}
-            viewBox={`0 0 256 256`}
-          />
+        <View className="space-y-2 mb-4 py-8">
+          <View className="flex-row space-x-2">
+            <View className="w-[50%] flex-1 flex-row justify-center items-center space-x-2 px-4 py-4 bg-white rounded-xl border border-[#EFEFEF]">
+              <Group color={"#000"} />
+              <Text>Mitglieder</Text>
+            </View>
+            <View className="w-[50%] flex-1 flex-row justify-center items-center space-x-2 px-4 py-4 bg-white rounded-xl border border-[#EFEFEF]">
+              <Settings color={"#000"} />
+              <Text>Einstellungen</Text>
+            </View>
+          </View>
+          <View className="flex-row space-x-2">
+            <View className="w-[50%] flex-1 flex-row justify-center items-center space-x-2 px-4 py-4 bg-white rounded-xl border border-[#EFEFEF]">
+              <Group color={"#000"} />
+              <Text>Academy</Text>
+            </View>
+            <View className="w-[50%] flex-1 flex-row justify-center items-center space-x-2 px-4 py-4 bg-white rounded-xl border border-[#EFEFEF]">
+              <Search color={"#000"} />
+              <Text>Clubsuche</Text>
+            </View>
+          </View>
         </View>
         <View className="bg-white rounded-3xl mb-4">
-          <View className="px-4 py-3 border-b border-gray-100">
-            <View className="flex flex-col justify-center items-center w-20 h-20 bg-gray-100 rounded-full mb-2">
-              <FontAwesome name="user" color={"#8E8E8E"} size={32} />
+          <View className="rounded-3xl overflow-hidden">
+            <View className="bg-black pt-[25%]"></View>
+            <View className="flex-row justify-start">
+              <View className="w-fit p-4 inline-block">
+                <Text className="font-bold text-2xl">Test 1</Text>
+                <View className="bg-[#EEEEEE] rounded-md p-1">
+                  <Text className="text-[10px] text-[#545454]">
+                    0/500 Mitglieder
+                  </Text>
+                </View>
+              </View>
             </View>
-            <Text className="font-bold text-lg">{user.username}</Text>
-            <Text className="text-xs text-[#8E8E8E]">{user.email}</Text>
           </View>
-          <Pressable className="flex flex-row items-center space-x-2 px-4 py-3 border-b border-gray-100">
-            <Ticket size={16} color={"#8E8E8E"} />
-            <Text className="text-[#8E8E8E]">Mitgliedschaft</Text>
-          </Pressable>
-          <Pressable className="flex flex-row items-center space-x-2 px-4 py-3">
-            <UserRound size={16} color={"#8E8E8E"} />
-            <Text className="text-[#8E8E8E]">Profil bearbeiten</Text>
-          </Pressable>
         </View>
       </View>
     </Container>
